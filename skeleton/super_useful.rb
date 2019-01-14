472f63b31557a79abffc230cbe54877329554fbb
Lycinus <sam.d.odonnell@gmail.com>
2019-01-14 14:47:22 -0800

    Finish Error handling project

---
+++ skeleton/super_useful.rb
@@ -0,0 +1,64 @@
+# PHASE 2
+def convert_to_int(str)
+  begin
+    Integer(str)
+  rescue ArgumentError
+    return nil
+  end
+end
+
+# PHASE 3
+FRUITS = ["apple", "banana", "orange"]
+
+def reaction(maybe_fruit)
+  if FRUITS.include? maybe_fruit
+    puts "OMG, thanks so much for the #{maybe_fruit}!"
+  else 
+    raise StandardError 
+  end 
+end
+
+def feed_me_a_fruit
+  puts "Hello, I am a friendly monster. :)"
+
+  puts "Feed me a fruit! (Enter the name of a fruit:)"
+  maybe_fruit = gets.chomp
+  reaction(maybe_fruit)
+rescue
+  puts "give me coffee"
+  maybe_coffee = gets.chomp
+  if maybe_coffee == "coffee"
+    retry
+  else
+    raise "I said give me coffee >:)"
+  end
+end  
+
+# PHASE 4
+class BestFriend
+  def initialize(name, yrs_known, fav_pastime)
+    @name = name
+    @yrs_known = yrs_known
+    @fav_pastime = fav_pastime
+
+    raise FriendshipError.new("you haven't known them for long enough") if @yrs_known < 5
+    raise FriendshipError.new("I don't know your name or hobbies!") if @name.length < 1 || @fav_pastime.length < 1
+  end
+
+  def talk_about_friendship
+    puts "Wowza, we've been friends for #{@yrs_known}. Let's be friends for another #{1000 * @yrs_known}."
+  end
+
+  def do_friendstuff
+    puts "Hey bestie, let's go #{@fav_pastime}. Wait, why don't you choose. 😄"
+  end
+
+  def give_friendship_bracelet
+    puts "Hey bestie, I made you a friendship bracelet. It says my name, #{@name}, so you never forget me." 
+  end
+end
+
+# custom errors
+class FriendshipError < StandardError; end
+
+
